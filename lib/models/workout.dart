e0e87e1c0dd7daec68bf593a29776964b4a36117
mrmrjing <hongjing123321@gmail.com>
2024-07-22 17:55:07 +0800

    Add userWeight and location as keys in the workout map

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -1,32 +1,74 @@
+import 'dart:convert';
+import 'exercise_detail.dart';
+import 'package:flutter/material.dart'; 
+
 class Workout {
-  final int? id; // Make id nullable to handle cases where it isn't set yet (e.g., before being inserted into the database)
-  final String date;
-  final String description;
+  int? id;
+  String date;
+  List<ExerciseDetail> exercises;
+  double? userWeight;  // Body weight on the day of the workout
+  String? location; // Optional field for tracking workout location
 
-  Workout({this.id, required this.date, required this.description});
-
-  // Adding a copy method
-  Workout copy({int? id, String? date, String? description}) {
-    return Workout(
-      id: id ?? this.id,
-      date: date ?? this.date,
-      description: description ?? this.description,
-    );
-  }
+  Workout({
+    this.id,
+    required this.date,
+    required this.exercises,
+    this.userWeight,
+    this.location,
+  });
 
   Map<String, dynamic> toMap() {
     return {
-      if (id != null) 'id': id, // Only add id to map if it is not null
+      'id': id,
       'date': date,
-      'description': description,
+      'exercises': jsonEncode(exercises.map((e) => e.toJson()).toList()),
+      'userWeight': userWeight,
+      'location': location,
     };
   }
 
-  static Workout fromMap(Map<String, dynamic> map) {
+  factory Workout.fromMap(Map<String, dynamic> map) {
+  print("Parsing Workout from Map: $map");
+
+  List<ExerciseDetail> parsedExercises = List<ExerciseDetail>.from(
+    jsonDecode(map['exercises']).map((x) => ExerciseDetail.fromMap(x as Map<String, dynamic>))
+  );
+  print("Parsed Exercises: ${parsedExercises.map((e) => e.description).join(', ')}");
+
+  double? parsedWeight = map['userWeight'] != null ? double.parse(map['userWeight'].toString()) : null;
+  String? parsedLocation = map['location'];
+
+  print("Parsed Weight: $parsedWeight");
+  print("Parsed Location: $parsedLocation");
+
+  return Workout(
+    id: map['id'],
+    date: map['date'],
+    exercises: parsedExercises,
+    userWeight: parsedWeight,
+    location: parsedLocation,
+  );
+}
+
+  factory Workout.fromJson(Map<String, dynamic> json) {
     return Workout(
-      id: map['id'],
-      date: map['date'],
-      description: map['description'],
+      id: json['id'],
+      date: json['date'],
+      exercises: List<ExerciseDetail>.from(
+        json['exercises'].map((x) => ExerciseDetail.fromJson(x))
+      ),
+      userWeight: json['userWeight'] != null ? json['userWeight'].toDouble() : null,
+      location: json['location'],
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'date': date,
+      'exercises': exercises.map((e) => e.toJson()).toList(),
+      'userWeight': userWeight,
+      'location': location,
+    };
   }
 }
