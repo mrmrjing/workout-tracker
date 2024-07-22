4e695f84357751e136c6323c714eeb00d899e74a
mrmrjing <hongjing123321@gmail.com>
2024-07-22 18:02:08 +0800

    Imported workout data as JSON from past workout history

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import '../models/workout.dart';
-import '../services/database_helper.dart';
-import '../views/add_workout_screen.dart';
+import 'package:sqflite/sqflite.dart';
+import 'views/workout_list_screen.dart'; 
+import 'services/database_helper.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  // Ensure proper initialization
+  print('Database path: ${await getDatabasesPath()}');
+  await DatabaseHelper.instance.importWorkoutsFromJson();  // Ensure database is initialized
   runApp(MyApp());
 }
 
@@ -16,77 +20,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: WorkoutListScreen(),
-    );
-  }
-}
-
-class WorkoutListScreen extends StatefulWidget {
-  @override
-  _WorkoutListScreenState createState() => _WorkoutListScreenState();
-}
-
-class _WorkoutListScreenState extends State<WorkoutListScreen> {
-  List<Workout> workouts = []; // Initialized to an empty list
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    refreshWorkouts();
-  }
-
-  @override
-  void dispose() {
-    DatabaseHelper.instance.close();
-    super.dispose();
-  }
-
-  Future refreshWorkouts() async {
-  setState(() => isLoading = true);
-  List<Workout> fetchedWorkouts = await DatabaseHelper.instance.readAllWorkouts();
-  workouts = fetchedWorkouts.where((workout) {
-    try {
-      DateTime.parse(workout.date); // Try parsing the date
-      return true; // Include if parsing is successful
-    } catch (e) {
-      return false; // Exclude if parsing fails
-    }
-  }).toList();
-  setState(() => isLoading = false);
-}
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('My Workouts'),
-      ),
-      body: isLoading
-          ? CircularProgressIndicator()
-          : workouts.isEmpty
-              ? Text('No workouts added.')
-              : ListView.builder(
-                  itemCount: workouts.length,
-                  itemBuilder: (context, index) {
-                    final workout = workouts[index];
-                    return ListTile(
-                      title: Text(workout.description),
-                      subtitle: Text(DateTime.parse(workout.date).toIso8601String()),
-                      onTap: () {
-                        // Implement onTap to view or edit workout
-                      },
-                    );
-                  },
-                ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => AddWorkoutScreen()),
-          ).then((_) => refreshWorkouts());
-        },
-      ),
     );
   }
 }
